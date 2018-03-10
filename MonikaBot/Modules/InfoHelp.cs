8c9787dcc8a90b1c67c2ad13b624aec8fdbd4d9c
Brian Vach <Brianvach@users.noreply.github.com>
2018-03-10 14:00:08 -0500

    Setup basic information for info and help

---
+++ MonikaBot/Modules/InfoHelp.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonikaBot.Modules
+{
+    class InfoHelp : ModuleBase<SocketCommandContext>
+    {
+        [Command("info")]
+        public async Task Info()
+        {
+            
+        }
+
+        [Command("help")]
+        public async Task DefaulHelp()
+        {
+
+        }
+    }
+}
